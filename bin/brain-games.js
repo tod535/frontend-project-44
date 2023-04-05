c1298f6a108776a2c722f2548f4e29009fd97ff1
tod535 <124677033+tod535@users.noreply.github.com>
2023-04-05 11:40:31 +0300

    4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,1 @@
-console.log('Welcome to the Brain Games!')
-
+console.log('Welcome to the Brain Games!');
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+export default function hello() {
   console.log('Welcome to the Brain Games!');
-  const username = readlineSync.question('May I have your name? John')
-    console.log(`Hello ${username}!`)
+  const username = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${username}!`);
 }
