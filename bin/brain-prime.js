44341846e48695a65756bec2672c7f5593f88b61
tod535 <124677033+tod535@users.noreply.github.com>
2023-04-23 14:08:30 +0300

    9

---
+++ bin/brain-prime.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import GamePrime from '../src/games/progression.js';
+
+GamePrime();
---
+++ src/games/prime.js
@@ -0,0 +1,23 @@
+import Game from '../index.js';
+import { GetRandomNumber } from '../utils.js';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no"';
+
+const isPrimeNum = (num) => {
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const getQuestionAndAnswer = () => {
+  const question = GetRandomNumber(2, 50);
+  const correctAnswer = isPrimeNum(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+export default function gemePrime() {
+  Game(description, getQuestionAndAnswer);
+}
