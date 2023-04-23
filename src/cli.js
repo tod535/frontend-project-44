b99cf8a278fdaca07ee611f418ac8df6db273d36
tod535 <124677033+tod535@users.noreply.github.com>
2023-04-23 15:20:56 +0300

    исправление 2

--- src/cli.js
+++ src/cli.js
@@ -3,5 +3,5 @@ import readlineSync from 'readline-sync';
 export default () => {
   console.log('Welcome to the Brain Games!');
   const username = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${username}!`);
+  console.log(`Hello, ${username}!`);
 };
