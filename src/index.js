589f7db2f69fe0031171303af08af3a7f6383c8c
tod535 <124677033+tod535@users.noreply.github.com>
2023-04-24 10:30:18 +0300

    10,2

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ export default (description, getQuestionAndAnswer) => {
 
   console.log(description);
 
-  for (let i = 1; i <= roundsCount; i += 1) {
+  for (let i = 0; i < roundsCount; i += 1) {
     const [question, correctAnswer] = getQuestionAndAnswer();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('You answer: ');
@@ -17,9 +17,9 @@ export default (description, getQuestionAndAnswer) => {
       console.log('Correct!');
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${userName}`);
+      console.log(`Let's try again, ${userName}!`);
       return;
     }
   }
-  console.log(` Congratulations, ${userName}`);
+  console.log(`Congratulations, ${userName}`);
 };
